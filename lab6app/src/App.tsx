807bc66d1ac0f007ab72fa114254eb12e58e4d44
LeafyGerenuk <manav_chakravarthy@brown.edu>
2022-04-09 19:17:12 -0400

    done

---
+++ lab6app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Horoscope from "./Horoscope";
+
+function App() {
+  return (
+      <div className="App">
+        <div className="Horoscope-view">Horoscope!</div>
+        <Horoscope></Horoscope>
+      </div>
+  );
+}
+
+export default App;
